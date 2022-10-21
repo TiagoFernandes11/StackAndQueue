dfd60f8336369dd912daacdb65c862a03378fb2c
Tiago Fernandes <tiagofernandesribeiro@yahoo.com.br>
2022-10-21 13:57:10 -0300

    minor fixing

--- stackQueue/src/main/java/StackExample.java
+++ stackQueue/src/main/java/StackExample.java
@@ -2,14 +2,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * @author User
- */
 public class StackExample {
 
     public static void main(String[] args) {
