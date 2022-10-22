8bfd4ec81739cda2273d79575e8c536b6a578b3c
Tiago Fernandes <tiagofernandesribeiro@yahoo.com.br>
2022-10-22 17:09:46 -0300

    Stack declaration and Pop Method demonstration

--- stackQueue/src/main/java/StacQueuekExample.java
+++ stackQueue/src/main/java/StacQueuekExample.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class StackExample {
+public class StacQueuekExample {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
---
+++ stackQueue/src/main/java/StackExemple.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+/**
+ *
+ * @author TiagoFernandes
+ */
+public class StackExemple {
+    
+    public static void main(String[] args) {
+        Stack<Integer> listaStack = new Stack<>();
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite um numero inteiro: ");
+            listaStack.add(sc.nextInt());
+        }
+        
+        System.out.println("\nImprimindo a lista: ");
+        
+        for (int i = 0; i < 5; i++) {
+            System.out.println(listaStack.pop());
+        }
+    }
+}
