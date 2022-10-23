e5c8b56690bc0ac8e8e099cedf0f67dcedeca731
Tiago Fernandes <tiagofernandesribeiro@yahoo.com.br>
2022-10-22 21:14:23 -0300

    minor fix

--- stackQueue/src/main/java/QueueExample.java
+++ stackQueue/src/main/java/QueueExample.java
@@ -9,16 +9,13 @@ import java.util.Scanner;
  * @author TiagoFernandes
  */
 public class QueueExample {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Queue<Integer> lista = new LinkedList<>();
-
         for (int i = 0; i < 5; i++) {
             System.out.println("Digite um numero inteiro: ");
             lista.add(sc.nextInt());
         }
-
         System.out.println("");
         System.out.println("Lista em Queue: ");
 
@@ -26,12 +23,10 @@ public class QueueExample {
             System.out.println(lista.remove());
         }
     }
-
     public static int queueInt(ArrayList<Integer> lista) {
         if (lista.size() >= 1) {
             return lista.remove(0);
         }
         return 0;
     }
-
 }
